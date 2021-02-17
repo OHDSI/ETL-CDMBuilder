9ce2f261937ae431561dc29611d01ed3f783149b
Anton Ivanov <antoha.ivanov@gmail.com>
2021-02-17 15:10:03 +0300

    connection updates

--- source/org.ohdsi.cdm.framework.desktop/Helpers/SqlConnectionHelper.cs
+++ source/org.ohdsi.cdm.framework.desktop/Helpers/SqlConnectionHelper.cs
@@ -92,7 +92,8 @@ namespace org.ohdsi.cdm.framework.desktop.Helpers
             {
                 var odbc = new OdbcConnectionStringBuilder(odbcConnectionString);
 
-               var connectionStringTemplate = "Server={server};Port=5432;Database={database};User Id={username};Password={password};SslMode=Require;Trust Server Certificate=true";
+               //var connectionStringTemplate = "Server={server};Port=5432;Database={database};User Id={username};Password={password};SslMode=Require;Trust Server Certificate=true";
+                var connectionStringTemplate = "Server={server};Port=5432;Database={database};User Id={username};Password={password};";
 
                 var npgsqlConnectionString = connectionStringTemplate.Replace("{server}", odbc["server"].ToString())
                     .Replace("{database}", odbc["database"].ToString()).Replace("{username}", odbc["uid"].ToString())

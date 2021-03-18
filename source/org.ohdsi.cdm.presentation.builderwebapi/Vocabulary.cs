dfc97609cf7026db107686c3bebd8c6aae8a6080
Anton Ivanov <antoha.ivanov@gmail.com>
2021-03-18 11:01:59 +0300

    Update Vocabulary.cs

--- source/org.ohdsi.cdm.presentation.builderwebapi/Vocabulary.cs
+++ source/org.ohdsi.cdm.presentation.builderwebapi/Vocabulary.cs
@@ -117,10 +117,10 @@ namespace org.ohdsi.cdm.presentation.builderwebapi
 
                                     var timer = new Stopwatch();
                                     timer.Start();
-
+                                                                        
                                     WriteLog(Status.Running, "Database " + _settings.VocabularyEngine.Database, 0);
                                     WriteLog(Status.Running, "1 " + _settings.VocabularyConnectionString, 0);
-                                    WriteLog(Status.Running, "2 " + _settings.VocabularyEngine.GetConnection(_settings.VocabularyConnectionString).ConnectionString, 0);
+                                    WriteLog(Status.Running, "2 " + SqlConnectionHelper.GetConnection(_settings.VocabularyConnectionString, _settings.VocabularyEngine.Database), 0);
                                     WriteLog(Status.Running, string.Format("{0}| {1}", DateTime.Now, conceptIdMapper.Lookup + " - Loading ..."), 0);
                                                                        
 

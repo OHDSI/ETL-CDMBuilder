b90541d71bf371ed312e868623c1e2a4aa19bfaa
Valeriy Sedov <VSedov@its.jnj.com>
2025-11-10 23:07:04 +0100

    Save info about Vendor primary key columns

--- source/org.ohdsi.cdm.presentation.builder/Utility/VendorHelper.cs
+++ source/org.ohdsi.cdm.presentation.builder/Utility/VendorHelper.cs
@@ -1,12 +1,7 @@
 ﻿using org.ohdsi.cdm.framework.common.Base;
 using org.ohdsi.cdm.framework.common.Enums;
 using org.ohdsi.cdm.framework.common.Utility;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EtlTransformation = org.ohdsi.cdm.framework.etl.Transformation;
 
 namespace org.ohdsi.cdm.presentation.builder.Utility
@@ -59,7 +54,32 @@ namespace org.ohdsi.cdm.presentation.builder.Utility
                 "Truven_MDCR" => new EtlTransformation.Truven.TruvenPersonBuilder(vendor),
                 "Truven_CCAE" => new EtlTransformation.Truven.TruvenPersonBuilder(vendor),
 
-                _ => throw new NotImplementedException("Failed to setup the PersonBuilder!")
+                _ => throw new NotImplementedException("Failed to recognize vendor!")
+            };
+        }
+
+        public static string GetVendorPrimaryKeyName()
+        {
+            var vendor = Settings.Current.Building.VendorToProcess;
+            return vendor.Name switch
+            {
+                "CDM" => "PERSON_ID",
+                "Cprd" => "patid",
+                "CprdAurum" => "patid",
+                "CprdHES" => "patid",
+                "Era" => "PERSON_ID",
+                "HealthVerity" => "hvid",
+                "OptumExtendedSES" => "patid",
+                "OptumExtendedDOD" => "patid",
+                "OptumPanther" => "ptid",
+                "JMDC" => "member_id",
+                "PregnancyAlgorithm" => "PERSON_ID",
+                "Premier" => "medrec_key",
+                "Truven_MDCD" => "ENROLID",
+                "Truven_MDCR" => "ENROLID",
+                "Truven_CCAE" => "ENROLID",
+
+                _ => throw new NotImplementedException("Failed to recognize vendor!")
             };
         }
     }
